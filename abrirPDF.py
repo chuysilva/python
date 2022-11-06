331526672e05832e8746fc518e0e6935ff8398ca
ChuyPc <albertosilvarincon@gmail.com>
2022-11-06 14:09:06 -0600

    primercommit

---
+++ abrirPDF.py
@@ -0,0 +1,3 @@
+import webbrowser as wb
+
+wb.open_new(r'C:\Users\alber\Desktop\Varios\FACU\Semestre 12\Taller 2\AnteProyecto.pdf')
---
+++ python.py
@@ -0,0 +1,12 @@
+import tkinter
+
+ventana = tkinter.Tk()  #Creacion de ventana
+ventana.geometry("400x300") #Redimensionamos ventava
+
+#Para agregar cosas, recibe dos parametros el primero es donde se va a mostrar y el segundo el texto que diga
+#etiqueta  = tkinter.Label(ventana, text = "Hola como estas? ", background = "blue")
+#Se agrega a la ventana con .pack() 
+#etiqueta.pack(fill = tkinter.X)
+
+ventana.mainloop()
+ 
